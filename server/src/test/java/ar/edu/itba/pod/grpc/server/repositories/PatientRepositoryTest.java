d128355a49f727bf3c269b77903bdcb887e950c1
PakiQuian <f.quianblanco@gmail.com>
2024-09-25 17:50:50 -0300

    UUID en testeo para nombres de pacientes y tests de PatientRepositoy

--- server/src/test/java/ar/edu/itba/pod/grpc/server/repositories/PatientRepositoryTest.java
+++ server/src/test/java/ar/edu/itba/pod/grpc/server/repositories/PatientRepositoryTest.java
@@ -1,12 +1,15 @@
 package ar.edu.itba.pod.grpc.server.repositories;
 
+import ar.edu.itba.pod.grpc.hospital.Patient;
 import ar.edu.itba.pod.grpc.server.exceptions.InvalidLevelException;
 import ar.edu.itba.pod.grpc.server.exceptions.PatientAlreadyExistsException;
 import ar.edu.itba.pod.grpc.server.exceptions.PatientDoesNotExistException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -17,8 +20,8 @@ public class PatientRepositoryTest {
 
     private static PatientRepository patientRepository;
 
-    private static final int THREAD_COUNT = 100;
-    private static final int PATIENTS_BY_THREAD = 100;
+    private static final int THREAD_COUNT = 1000;
+    private static final int PATIENTS_BY_THREAD = 1000;
     private static final int EXPECTED_PATIENTS = THREAD_COUNT * PATIENTS_BY_THREAD;
 
     private final Random r = new Random();
@@ -43,6 +46,15 @@ public class PatientRepositoryTest {
     }
 
     @Test
+    void updateLevelTest() {
+        Patient p1 = patientRepository.addPatient("Martin", 5);
+        Patient p2 = patientRepository.updateLevel("Martin", 4);
+
+        assertEquals(p1.getName(), p2.getName());
+        assertEquals(4, p2.getLevel());
+    }
+
+    @Test
     void getPatientsTest() {
         assertDoesNotThrow(() -> patientRepository.addPatient("Martin", 5));
         assertDoesNotThrow(() -> patientRepository.addPatient("Marcos", 4));
@@ -57,6 +69,20 @@ public class PatientRepositoryTest {
     }
 
     @Test
+    void getFirstPatientFromEveryLevelTest() {
+        patientRepository.addPatient("A", 5);
+        patientRepository.addPatient("B", 5);
+        patientRepository.addPatient("C", 3);
+        patientRepository.addPatient("D", 3);
+
+        List<Patient> patients = patientRepository.getFirstPatientFromEveryLevel();
+
+        assertEquals(2, patients.size());
+        assertEquals("A", patients.getFirst().getName());
+        assertEquals("C", patients.getLast().getName());
+    }
+
+    @Test
     void checkPatientTest() {
         assertDoesNotThrow(() -> patientRepository.addPatient("Lucas", 5));
         assertDoesNotThrow(() -> patientRepository.addPatient("Marcos", 4));
@@ -64,6 +90,25 @@ public class PatientRepositoryTest {
         assertDoesNotThrow(() -> patientRepository.addPatient("Julio", 1));
         assertEquals(2, patientRepository.checkPatient("Juan").getQueueLength());
         assertThrows(PatientDoesNotExistException.class, () -> patientRepository.checkPatient("Jose"));
+    }
+
+    @Test
+    void checkAndUpdatePatientTest() {
+        patientRepository.addPatient("Primero", 1);
+        patientRepository.addPatient("Segundo", 1);
+        assertEquals(0, patientRepository.checkPatient("Primero").getQueueLength());
+        assertEquals(1, patientRepository.checkPatient("Segundo").getQueueLength());
+
+        patientRepository.updateLevel("Primero", 2);
+        patientRepository.addPatient("Tercero", 2);
+        assertEquals(0, patientRepository.checkPatient("Primero").getQueueLength());
+        assertEquals(1, patientRepository.checkPatient("Tercero").getQueueLength());
+        assertEquals(2, patientRepository.checkPatient("Segundo").getQueueLength());
+
+        patientRepository.updateLevel("Segundo", 2);
+        assertEquals(0, patientRepository.checkPatient("Primero").getQueueLength());
+        assertEquals(1, patientRepository.checkPatient("Segundo").getQueueLength());
+        assertEquals(2, patientRepository.checkPatient("Tercero").getQueueLength());
     }
 
     @Test
@@ -82,7 +127,7 @@ public class PatientRepositoryTest {
 
         Runnable addition = () -> {
             for (int i = 0; i < PATIENTS_BY_THREAD; i++) {
-                patientRepository.addPatient("Patient" + r.nextInt(), r.nextInt(1, 5));
+                patientRepository.addPatient("Patient" + UUID.randomUUID(), r.nextInt(1, 5));
             }
         };
 
@@ -92,7 +137,7 @@ public class PatientRepositoryTest {
             }
         } finally {
             executor.shutdown();
-            executor.awaitTermination(2, TimeUnit.SECONDS);
+            executor.awaitTermination(20, TimeUnit.SECONDS);
         }
 
         assertEquals(EXPECTED_PATIENTS, patientRepository.getPatients().size());
@@ -122,5 +167,4 @@ public class PatientRepositoryTest {
 
         assertEquals(1, patientRepository.getPatients().size());
     }
-
 }
